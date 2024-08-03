f6251f8ac1568c93b79c4801813d289e8a266fca
AceStella <AceStella@foxmail.com>
2024-08-03 14:38:24 +0800

    240803, cats finished

---
+++ class/4_1_list_partition.py
@@ -0,0 +1,23 @@
+def list_partitions(n, m):
+    """List partitions.
+    
+    >>> for p in list_partitions(6, 4): print(p)
+    [2, 4]
+    [1, 1, 4]
+    [3, 3]
+    [1, 2, 3]
+    [1, 1, 1, 3]
+    [2, 2, 2]
+    [1, 1, 2, 2]
+    [1, 1, 1, 1, 2]
+    [1, 1, 1, 1, 1, 1]
+    """
+    if n < 0 or m == 0:
+        return []
+    else:
+        exact_match = []
+        if n == m:
+            exact_match = [[m]]
+        with_m = [p + [m] for p in list_partitions(n-m, m)]
+        without_m = list_partitions(n, m-1)
+        return exact_match + with_m + without_m
